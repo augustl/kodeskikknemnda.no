6ac3c9c427f037af1f432333d46f5134c11f40d1
August Lilleaas <august@augustl.com>
2019-07-11 18:37:26 +0300

    Glorious progress when building site

--- src/utviklingslandet_no/cli.clj
+++ src/utviklingslandet_no/cli.clj
@@ -7,8 +7,12 @@
 (def optimize optimizations/all)
 
 (defn build-site [dir]
+  (prn "Get assets")
   (let [assets (optimize (web/get-assets) {})]
     (stasis/empty-directory! dir)
+    (prn "Save assets")
     (optimus.export/save-assets assets dir)
+    (prn "Export pages")
     (stasis/export-pages (web/get-pages) dir {:optimus-assets assets
-                                              :base-url "https://utviklingslandet.no"})))
+                                              :base-url "https://utviklingslandet.no"})
+    (prn "Done!")))
