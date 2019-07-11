6f9ce3a8cf7a3c3565fbf1320bc8c2b687c14aa9
August Lilleaas <august@augustl.com>
2019-07-11 18:30:43 +0300

    Home page with link tag to RSS in <head>

--- src/utviklingslandet_no/web.clj
+++ src/utviklingslandet_no/web.clj
@@ -5,11 +5,21 @@
             [optimus.strategies :as strategies]
             [optimus.optimizations :as optimizations]
             [ring.middleware.content-type :refer [wrap-content-type]]
+            [hiccup.page]
             [prone.middleware :as prone]
             [utviklingslandet-no.rss :as rss]))
 
+(defn get-home-page []
+  (hiccup.page/html5
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:title "Utviklingslandet"]
+     [:link {:type "application/rss+xml" :rel "alternate" :title "utviklingslandet.no" :href "https://utviklingslandet.no/rss.xml"}]]
+    [:body
+     "Bør vi lage en web egentlig? Vi er ikke sikre, derfor har du fått denne stygge redirecten til backblaze sin b2 bucket."]))
+
 (defn get-pages []
-  {"/" "Bør vi lage en web egentlig? Vi er ikke sikre, derfor har du fått denne stygge redirecten til backblaze sin b2 bucket."
+  {"/" (get-home-page)
    "/rss.xml" (rss/generate-rss (-> "episodes.edn" clojure.java.io/resource slurp clojure.edn/read-string))})
 
 (defn get-assets []
