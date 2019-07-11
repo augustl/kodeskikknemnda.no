e2e8086b8fe9b337fff436f1d90f677e7f7b2311
August Lilleaas <august@augustl.com>
2019-07-11 18:26:49 +0300

    Replicate home page text

--- src/utviklingslandet_no/web.clj
+++ src/utviklingslandet_no/web.clj
@@ -9,13 +9,23 @@
             [utviklingslandet-no.rss :as rss]))
 
 (defn get-pages []
-  {"/" "Hello, world!"
+  {"/" "Bør vi lage en web egentlig? Vi er ikke sikre, derfor har du fått denne stygge redirecten til backblaze sin b2 bucket."
    "/rss.xml" (rss/generate-rss (-> "episodes.edn" clojure.java.io/resource slurp clojure.edn/read-string))})
 
 (defn get-assets []
   (assets/load-assets "public" [#".*"]))
 
+(defn wrap-content-type-utf-8 [handler]
+  (fn [request]
+    (when-let [response (handler request)]
+      (if (.contains (get-in response [:headers "Content-Type"]) ";")
+        response
+        (if (string? (:body response))
+          (update-in response [:headers "Content-Type"] #(str % "; charset=utf-8"))
+          response)))))
+
 (def app (-> (stasis/serve-pages get-pages)
              (optimus/wrap get-assets optimizations/none strategies/serve-live-assets)
              wrap-content-type
+             wrap-content-type-utf-8
              prone/wrap-exceptions))
