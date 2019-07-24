2b4c6c75937d0220ac17f103e49ced5a9bf57d3e
August Lilleaas <august@augustl.com>
2019-07-24 16:50:58 +0200

    Remove old build scripts

--- build-firebase.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-lein build-site
-firebase deploy
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-lein build-site
-
-# We don't want to overwrite existing episode files, so this is the current hackaround
-rclone copyto dist/rss.xml utv:utviklingslandet-public/rss.xml
-rclone copyto dist/index.html utv:utviklingslandet-public/index.html
